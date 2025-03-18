87737e14e6102901d6440af261cb7e40bdb3ddfa
SamuelRicardos <samuelric4rdo@gmail.com>
2025-03-18 17:50:16 -0300

    alteracoes no About e implementacao de animacoes do Aos

--- src/app/_components/hero.tsx
+++ src/app/_components/hero.tsx
@@ -20,19 +20,25 @@ export function Hero() {
             <div className='container mx-auto pt-16 pb-16 md:pb-0 py-16 px-4 relative'>
                 <article className='grid grid-cols-1 lg:grid-cols-2 gap-8'>
                     <div className='space-y-6'>
-                        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-10">
+                        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-10"
+                        data-aos="fade-down"
+                        >
                             Seu pet merece cuidado, carinho e atenção especial
                         </h1>
-                        <p className="lg:text-lg">
+                        <p className="lg:text-lg" data-aos="fade-right">
                             Oferecemos os melhores serviços para garantir o bem-estar e a felicidade do seu amigo de quatro patas.
                         </p>
 
-                        <div>
-                            <a className="bg-green-500 px-5 py-2 rounded-md font-semibold flex items-center justify-center w-fit gap-2" href="#">
+                        
+                            <a className="bg-green-500 px-5 py-2 rounded-md font-semibold flex items-center justify-center w-fit gap-2" 
+                            data-aos="fade-up"
+                            data-aos-delay="500"
+                            target='_blank'
+                            href={`https://wa.me/40028922?text=Olá vim pelo site e gostaria de mais informações`}>
                                 <WhatsappLogo className='w-5 h-5'></WhatsappLogo>
                                Contato via WhatsApp 
                             </a>
-                        </div>
+                        
                         <div className="mt-8">
                             <p className="text-sm mb-4">
                                 <b className="bg-black text-white px-2 py-1 rounded-md">5%</b> de desconto na primeira compra.
